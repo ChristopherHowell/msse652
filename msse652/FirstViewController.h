06d8f4a8026539aff596003bb1e2587673a8d850
Christopher Howell <howel783@regis.edu>
2014-07-06 00:29:48 -0700

    Week 1 Assignment
    Here is my week 1 project.

---
+++ msse652/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  msse652
+//
+//  Created by Christopher Howell on 7/5/14.
+//  Copyright (c) 2014 msse652. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+- (IBAction)exitHere:(UIStoryboardSegue *)sender;
+
+@end
---
+++ msse652/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  msse652
+//
+//  Created by Christopher Howell on 7/5/14.
+//  Copyright (c) 2014 msse652. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
